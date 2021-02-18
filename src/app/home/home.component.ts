832d115c27892bdb3666a97caaeca33ddc2c758a
Boryszs <12damie@gmail.com>
2021-02-18 01:19:48 +0100

    update home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {ServiceService} from '../services/service.service';
+import { DataSource } from '@angular/cdk/table';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  dataSourceCurrencyC;
+  dataSourceCurrency;
+  dataSourceExchangePl;
+  dataSourceExchangeW;
+  displayedColumnsCurrencyC: string[] = ['name', 'course', 'value', 'change'];
+  displayedColumnsCurrency: string[] = ['country', 'name', 'symbol', 'exchange','percentage change'];
+  displayedExchange: string[] = ['name', 'course', 'change', 'changePercentages','open','max','min','time'];
+  constructor(private service :ServiceService) { }
 
   ngOnInit(): void {
+    this.service.crypto5Importnant().subscribe(data => this.dataSourceCurrencyC = data)
+    this.service.currency5All().subscribe(data => this.dataSourceCurrency = data)
+    this.service.exchange5PlAll().subscribe(data => this.dataSourceExchangePl = data)
+    this.service.exchange5WAll().subscribe(data => this.dataSourceExchangeW = data)
   }
 
 }
--- src/app/services/service.service.ts
+++ src/app/services/service.service.ts
@@ -15,17 +15,34 @@ export class ServiceService {
     return this.http.get<any[]>(BASE_URL+"exchange/pl/all"); 
   }
 
+  exchange5PlAll(){
+    return this.http.get<any[]>(BASE_URL+"exchange/pl/all/5"); 
+  }
+
   exchangeWAll(){
     return this.http.get<any[]>(BASE_URL+"exchange/w/all"); 
+  }
+
+  exchange5WAll(){
+    return this.http.get<any[]>(BASE_URL+"exchange/w/all/5"); 
   }
 
   cryptoImportnant(){
     return this.http.get<any[]>(BASE_URL+"crypt-currency/important"); 
   }
 
+  crypto5Importnant(){
+    return this.http.get<any[]>(BASE_URL+"crypt-currency/important/5"); 
+  }
+
   currencyAll(){
     return this.http.get<any[]>(BASE_URL+"currency/all"); 
   }
+
+  currency5All(){
+    return this.http.get<any[]>(BASE_URL+"currency/all/5"); 
+  }
+
 }
 
 
